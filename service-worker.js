2295683c0d9af1480fb4d227ae11ae6ae0aa54a1
shigimcp <shigimcp@hotmail.com>
2020-11-27 15:46:10 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shigimcp2020-react/precache-manifest.8fcea54264a9344f2caba4fa97237e98.js"
+  "/shigimcp2020-react/precache-manifest.bf84f9a18a529b92d363213559e5c19e.js"
 );
 
 self.addEventListener('message', (event) => {
